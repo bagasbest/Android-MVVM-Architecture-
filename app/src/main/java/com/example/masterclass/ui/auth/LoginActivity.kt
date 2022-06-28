a0640bd9c13609fea3a0d4bbcf0c96d8ca89ed58
Bagas Pangestu <bagasbest80@gmail.com>
2022-06-28 17:25:54 +0700

    login & register

--- app/src/main/java/com/example/masterclass/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/masterclass/ui/auth/LoginActivity.kt
@@ -2,9 +2,11 @@ package com.example.masterclass.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.masterclass.databinding.ActivityLoginBinding
+import com.example.masterclass.utils.toast
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), AuthListener {
 
     private var _binding : ActivityLoginBinding? = null
     private val binding get() = _binding!!
@@ -13,13 +15,26 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val viewModel = ViewModelProvider(this)[AuthViewModel::class.java]
+        binding.viewModel = viewModel
 
-
-
+        viewModel.authListener = this
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    override fun onStarted() {
+        toast("Login Started")
+    }
+
+    override fun onSuccess() {
+        toast("Login Success")
+    }
+
+    override fun onFailure(message: String) {
+        toast(message)
     }
 }
